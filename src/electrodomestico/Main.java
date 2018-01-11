ac08853eb839cd4493b3a1eab698e184bbba2db1
german <german@DESKTOP-079T225>
2018-01-11 23:07:14 +0100

    2DO

--- src/electrodomestico/Main.java
+++ src/electrodomestico/Main.java
@@ -15,11 +15,19 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Electrodomestico nevera=new Electrodomestico(1,"morado",'h',1);
-        
-        System.out.println(nevera.getColor());
-        String a="\nblANco";
-        System.out.println(a.toLowerCase());
+        Electrodomestico[] nevera = new Electrodomestico[10];
+        nevera[0] = new Electrodomestico(3, "azul", 'A', 11);
+        nevera[1] = new Electrodomestico(4, "VERDE", 'B', 22);
+        nevera[2] = new Electrodomestico(5, "rojo", 'E', 33);
+        //
+        for (int i = 0; i < nevera.length; i++) {
+            if (nevera[i] != null) {
+                System.out.println(nevera[i].getColor().toUpperCase());
+                System.out.println(nevera[i].getConsumoEnergetico());
+                System.out.println(nevera[i].getPeso());
+                System.out.println(nevera[i].precioFinal()+"\n");
+           }
+        }
     }
-    
+
 }
